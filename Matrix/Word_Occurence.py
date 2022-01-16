0875e65cc7292c94a6a3ba6d8c5b12dc6d273511
Boom-Ba <70102254+Boom-Ba@users.noreply.github.com>
2022-01-16 16:23:06 -0500

    Update Word_Occurence.py

--- Matrix/Word_Occurence.py
+++ Matrix/Word_Occurence.py
@@ -1,35 +1,42 @@
-##problem is to find all occurences word in mat
-def isValid(i,j,path,mat):
-  return 0<=i<len(mat) and 0<=j<len(mat[0]) and mat[i][j] not in path
 
-def dfs(mat,word,i,j,res,index=0,path=[]):
-  if mat[i][j]!=word[index]: #skip
-    return 
-  path.append((i,j))
-  ##get complete word 
-  if index==len(word)-1:
-    res.append(path.copy())
-  else:
-    for nr,nc in [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]:
-      if isValid(nr,nc,path,mat):
-        dfs(mat,word,nr,nc,res,index+1,path)
+def valid(x,y,path,mat):
+  return (0 <= x < len(mat)) and (0 <= y < len(mat[0])) and (x, y) not in path
 
-if __name__ == '__main__':
- 
-    mat = [
+def searchMatrix(mat, word):
+  if not mat or not word:
+    return res
+  
+  def dfs(i,j,mat,path,index,word):
+    r=[-1,0,1,-1, 0,1,1,1]
+    c=[-1,1,0, 0,-1,0,0,1]
+
+    if index==len(word):
+      res.add(tuple(path))
+      return 
+    if mat[i][j] !=word[index]:
+      return 
+    
+    path.append((i,j))
+    for k in range(len(r)):
+      if valid(i+r[k],j+c[k],path,mat):
+        dfs(i+r[k],j+c[k],mat,path,index+1,word)
+    path.pop()
+
+  m,n=len(mat),len(mat[0])
+  path=[]
+  res=set()
+  for i in range(m):
+    for j in range(n):
+      dfs(i,j,mat,path,0,word)
+  
+  return res
+if __name__=='__main__':
+  mat = [
         ['D', 'E', 'M', 'X', 'B'],
         ['A', 'O', 'E', 'P', 'E'],
         ['D', 'D', 'C', 'O', 'D'],
         ['E', 'B', 'E', 'D', 'S'],
         ['C', 'P', 'Y', 'E', 'N']
     ]
-    m,n =len(mat),len(mat[0])
-    word = 'CODE'
-  
-    res=[]
-    for i in range(m):
-      for j in range(n):
-        dfs(mat, word, i,j, res)
-    print(res)
-    
- 
+  word = 'CODE'
+ ret =searchMatrix(mat, word)
